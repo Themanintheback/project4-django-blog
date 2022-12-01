1349e6425c274b4ff899eb3ca51f4f6bd69f9aba
Themanintheback <jonathan.john2501@hotmail.co.uk>
2022-12-01 05:02:43 +0000

    add the ability to upload images to blog post

--- blog/form.py
+++ blog/form.py
@@ -12,7 +12,7 @@ for item in choices:
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'category', 'body', 'snippet')
+        fields = ('title', 'title_tag', 'category', 'body', 'snippet', 'header_image')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title Name Here'}),
--- blog/models.py
+++ blog/models.py
@@ -15,8 +15,17 @@ class Category(models.Model):
         return reverse('home')
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    header_image = models.ImageField(null=True, blank=True, upload_to="media/")
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = RichTextField(blank=True, null=True)
